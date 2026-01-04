3221e56b580d5ad98f98954ed4efccac87e63f2e
Bruno Amaral <141674554+amaralBruno27866@users.noreply.github.com>
2026-01-04 14:56:01 +0000

    adiciona a definição da classe Spell no arquivo spell.h

---
+++ 3d&t/src/Magias/spell.h
@@ -0,0 +1,14 @@
+#ifndef SPELL_H
+#define SPELL_H
+
+#include <string>
+#include <iomanip>
+
+using namespace std;
+namespace tokioDefenders {
+    class Spell {
+        private:
+    }
+}
+
+#endif // SPELL_H
